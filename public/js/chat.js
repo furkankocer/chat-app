b56d3e7771cadb62b821253e43c52a9bee396610
furkankocer <furkankocer@hotmail.com.tr>
2020-03-03 18:06:40 +0300

    Form and button states added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,26 @@
 const socket = io()
 
+//Elements
+const $messageForm = document.querySelector("#message-form")
+const $messageFormInput = $messageForm.querySelector("input")
+const $messageFormButton = $messageForm.querySelector("button")
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    $messageFormButton.setAttribute("disabled","disabled") // Buton disabled oluyor.
+
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message, (message) => {
-        console.log("The message was delivered", message)
+    
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute("disabled")
+        if(error){
+            return console.log(error)
+        }
     })
 })
 
