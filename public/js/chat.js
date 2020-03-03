c6fecdbc7fea6288165cf82d9d8db04a1ed1ad28
furkankocer <furkankocer@hotmail.com.tr>
2020-03-03 16:54:48 +0300

    code fixed

---
+++ public/js/chat.js
@@ -0,0 +1,24 @@
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
+})
+
+document.querySelector("#send-location").addEventListener("click", () => {
+    if(!navigator.geolocation)
+        return alert("Geolocation is not supported by your browser")
+    
+        navigator.geolocation.getCurrentPosition((positon) =>{
+            socket.emit("sendLocation", {
+                latitude:positon.coords.latitude,
+                longitude:positon.coords.longitude
+            })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
-const path = require("path");
-const express = require("express");
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
 
-const app = express();
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirectoryPath)); //app.use('/static', express.static('public'))
+app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
-  console.log("Server is up on port !");
-});
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+    
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit("message", "A user has been join")
+
+    socket.on("disconnect", () =>{
+        io.emit("message", "A user has left!")
+    })
+    socket.on("sendLocation", (coords) => {
+        const location = {
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        }
+        io.emit("message",location)
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
